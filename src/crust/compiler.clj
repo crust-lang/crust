47fc174f0c959b50537280c071ba74c956fce7d1
Geoff Shannon <geoffpshannon@gmail.com>
2017-04-08 18:58:34 -0500

    Add an emit-constant multimethod

--- src/crust/compiler.clj
+++ src/crust/compiler.clj
@@ -1,14 +1,23 @@
 (ns crust.compiler
   (:require [clojure.string :as str]))
 
+(defonce namespaces (atom {}))
+
+(defmulti emit-constant class)
+(defmethod emit-constant nil [x] "()")
+(defmethod emit-constant Long [x] (str x))
+(defmethod emit-constant Double [x] (str x))
+(defmethod emit-constant String [x] (pr-str x))
+(defmethod emit-constant Boolean [x] (if x "true" "false"))
+
 (defmulti emit :op)
+
 (defmethod emit :do [ast]
   (str "{ " (str/join "; " (map emit (conj (:statements ast) (:ret ast)))) " }"))
 
-(defmethod emit :const [ast]
-  (if (= (:type ast) :nil)
-    "()"
-    (pr-str (:val ast))))
+(defmethod emit :const
+  [{:keys [form]}]
+  (emit-constant form))
 
 (defmethod emit :if [ast]
   (str "if " (emit (:test ast)) " { "
