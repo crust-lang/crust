d4d7509a3281a6db538ed807d0fd8fdbc910ba06
Geoff Shannon <geoffpshannon@gmail.com>
2017-04-10 03:40:11 -0500

    Change alias for jvm analyzer

--- test/crust/compiler_test.clj
+++ test/crust/compiler_test.clj
@@ -1,40 +1,40 @@
 (ns crust.compiler-test
   (:require [clojure.test :refer :all]
-            [clojure.tools.analyzer.jvm :as jv]
+            [clojure.tools.analyzer.jvm :as jvm]
             [crust.compiler :refer :all]))
 
 (deftest rs-output-test
   (testing "constants"
-    (is (= "()" (emit (jv/analyze nil))))
-    (is (= "\"hello\"" (emit (jv/analyze "hello"))))
-    (is (= "1" (emit (jv/analyze 1)))))
+    (is (= "()" (emit (jvm/analyze nil))))
+    (is (= "\"hello\"" (emit (jvm/analyze "hello"))))
+    (is (= "1" (emit (jvm/analyze 1)))))
 
   (testing "special forms"
     (testing "if"
-      (is (= "if true { 1 } else { () }" (emit (jv/analyze '(if true 1)))))
-      (is (= "if true { 1 } else { 2 }" (emit (jv/analyze '(if true 1 2))))))
+      (is (= "if true { 1 } else { () }" (emit (jvm/analyze '(if true 1)))))
+      (is (= "if true { 1 } else { 2 }" (emit (jvm/analyze '(if true 1 2))))))
 
     (testing "do"
       (is (= "{ if true { 1 } else { 2 }; if false { 1 } else { 2 } }"
-             (emit (jv/analyze '(do (if true 1 2) (if false 1 2)))))))
+             (emit (jvm/analyze '(do (if true 1 2) (if false 1 2)))))))
 
     (testing "fn"
       (is (= "|x__0| { x__0 }"
-             (emit (jv/analyze '(fn [x] x)))))
+             (emit (jvm/analyze '(fn [x] x)))))
 
       (is (= "|x__0,y__0| { x__0 }"
-             (emit (jv/analyze '(fn [x y] x))))))
+             (emit (jvm/analyze '(fn [x y] x))))))
 
     (testing "let"
       (is (= "{ let x__0 = 10; x__0 }"
-             (emit (jv/analyze '(let [x 10] x))))))
+             (emit (jvm/analyze '(let [x 10] x))))))
 
     (testing "invoke"
       (is (= "{ let id__0 = |x__0| { x__0 }; id__0(()) }"
-             (emit (jv/analyze '(let [id (fn [x] x)]
+             (emit (jvm/analyze '(let [id (fn [x] x)]
                                   (id nil)))))))
 
     (testing "set!"
       #_(is (= "{ let mut x__0 = 10; x = 5; }"
-             (emit (jv/analyze '(let [^:mut x 10]
+             (emit (jvm/analyze '(let [^:mut x 10]
                                   (set! x 5)))))))))
