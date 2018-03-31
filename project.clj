653a210a4f385d0f535ab76591eb219f11b71156
Geoff Shannon <geoffpshannon@gmail.com>
2018-03-31 11:40:56 -0700

    Add lein-auto plugin

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.analyzer "0.6.9" :scope "test"]
-                 [org.clojure/tools.analyzer.jvm "RELEASE" :scope "test"]])
-
+                 [org.clojure/tools.analyzer.jvm "RELEASE" :scope "test"]]
+  :plugins [[lein-auto "0.1.3"]])
